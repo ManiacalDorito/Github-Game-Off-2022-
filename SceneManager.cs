036c3677686231463bd0efd2acb43adf4c4cbc6c
Kade Johnston <97554711+ManiacalDorito@users.noreply.github.com>
2022-11-05 16:59:59 -0600

    added error handlers to stream opening

--- SceneManager.cs
+++ SceneManager.cs
@@ -24,18 +24,42 @@ namespace Game_Jam_Game
 
         public void loadScene(string fileName)
         {
-            Stream stream;
-            stream = File.OpenRead(fileName);
+            Stream stream = null;
+
+            // Catches errors if the scene name is mispelled or the file cant be found
+            try
+            {
+                stream = File.OpenRead(fileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
             JsonSerializerOptions options = new JsonSerializerOptions();
-            //currentScene = JsonSerializer.Deserialize(stream, Scene, options);
+
+            // Deserializes the saved scene and puts it into currentScene to be loaded and shit
+            currentScene = JsonSerializer.Deserialize<Scene>(stream, options);
 
         }
 
         public void saveScene(string fileName)
         {
-            Stream stream;
-            stream = File.OpenWrite(fileName);
+            Stream stream = null;
+
+            // Catches errors if the scene name is mispelled or the file cant be found
+            try
+            {
+                stream = File.OpenWrite(fileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
             JsonSerializerOptions options = new JsonSerializerOptions();
+
+            // Saves current scene to file specified for loading later  
             JsonSerializer.Serialize(stream, currentScene, options);
             Console.Write("Saved to: " + stream);
             stream.Close();
